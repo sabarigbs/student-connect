5816aa51a7d9a09e81b8556a78e2693fe199d007
sabarigbs <sabarigovindarajan@gmail.com>
2018-08-02 23:37:26 +0530

    Updated Input component for Materialize

--- client/src/Components/InputComponent/Input.js
+++ client/src/Components/InputComponent/Input.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import './Input.css';
 
+class Input extends Component {
+    render() {
+        return (
+            <div className="input-field col s6">
+               
+                <input 
+                id={this.props.id} 
+                type={this.props.type} 
+                className={this.props.className} 
+                placeholder={this.props.placeholder}
+                onChange={this.props.onChange}></input>
+                
+                <label 
+                htmlFor={this.props.id}>
+                {this.props.name}
+                </label>
+            
+            </div>
+        );
+    }
+}
 
-export default class Input extends Component {
-  render() {
-    return (
-      <div className="form-group">
-        <label className="form-label" >{this.props.name}</label>
-        <input className="form-control" placeholder={this.props.placeholder} onChange={this.props.onChange} type={this.props.type}></input>
-      </div>
-    )
-  }
-};
+export default Input;
