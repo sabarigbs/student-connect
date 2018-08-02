ac85f0040d44a958b45b0a03f8318997d603db17
sabarigbs <sabarigovindarajan@gmail.com>
2018-08-02 23:35:46 +0530

    Updated App.js with Form Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
 import Form from './Components/FormComponent/Form';
+
+
 
 
 class App extends Component {
 
   render() {
     return (
-      <div className="container">
-        <Form />
-      </div>
+      
+        <Form/>
+      
     );
   }
 }
