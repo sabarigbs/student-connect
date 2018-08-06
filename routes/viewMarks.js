69675aa965305273ec24105f194f0606055901da
sabarigbs <sabarigovindarajan@gmail.com>
2018-08-06 08:35:23 +0530

    Fetched marks

--- routes/viewMarks.js
+++ routes/viewMarks.js
@@ -4,11 +4,27 @@ var db = require('./databaseConnection');
 
 
 router.post('/', function(req, response, next) {
+
+    var message;
+
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Content-Type', 'application/json');
     
     var username = req.body.username;
-      
-    db.query('',function(err,res,feilds){
 
+    console.log(username);
+      
+    db.query('SELECT student_name,course_id,course_name,marks FROM students NATURAL JOIN enrollments NATURAL JOIN courses NATURAL JOIN marks WHERE student_id = ? ',[username],function(err,res,feilds){
+        if(err)
+            throw err;
+        message={
+            'success':true,
+            'rollno':username,
+            'studentName':res[0]['student_name'],
+            'marks':res
+        }
+        console.log(res)
+        response.send(message);
     }); 
       
    
